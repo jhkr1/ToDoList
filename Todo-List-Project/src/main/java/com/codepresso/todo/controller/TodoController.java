24eaa5ade63ca05b1cec6bcff8d778095afd074e
Jihun <wlgjs06061@naver.com>
2023-07-23 18:28:34 +0900

    Feat:작성, 조회, 삭제

--- Todo-List-Project/src/main/java/com/codepresso/todo/controller/TodoController.java
+++ Todo-List-Project/src/main/java/com/codepresso/todo/controller/TodoController.java
@@ -2,34 +2,39 @@ package com.codepresso.todo.controller;
 
 import java.util.List;
 
+import com.codepresso.todo.service.TodoService;
 import com.codepresso.todo.vo.ResultDto;
 import com.codepresso.todo.vo.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class TodoController {
 
-    //todo TodoService를 활용하기 위해 의존성 주입 코드를 작성하세요
+    private final TodoService todoService;
 
-    //todo API문서를 확인하여 GET /todo REST API를 완성하세요
-    //todo TodoService를 활용하세요
+    public TodoController(TodoService todoService){
+        this.todoService = todoService;
+    }
+
+    @GetMapping("/todo")
     public List<Todo> getTodoList(){
-        List<Todo> list=null;
+        List<Todo> list= todoService.getTodoList();
         return list;
     }
 
-    //todo API문서를 확인하여 POST /todo REST API를 완성하세요
-    //todo TodoService를 활용하세요
-    public ResultDto addTodo(Todo todo) {
+    @PostMapping("/todo")
+    public ResultDto addTodo(@RequestBody Todo todo) {
+        todoService.addTodo(todo);
 
-        //todo return code는 변경하지 마세요
         return new ResultDto(200, "Success");
     }
 
-    //todo API문서를 확인하여 DELETE /todo REST API를 완성하세요
-    //todo TodoService를 활용하세요
+    @DeleteMapping("/todo/{id}")
     public ResultDto deleteTodo(@PathVariable("id") Integer id) {
+        todoService.deleteTodo(id);
 
-        //todo return code는 변경하지 마세요
         return new ResultDto(200, "Success");
     }
 
